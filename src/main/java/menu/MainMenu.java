6195b899a6c6cd872271fec55b481f617cba962a
Lybrica <karl.miikael100@gmail.com>
2021-08-07 13:20:35 +0300

    Initial commit

---
+++ src/main/java/menu/MainMenu.java
@@ -0,0 +1,19 @@
+package menu;
+
+import java.util.Scanner;
+
+public class MainMenu {
+
+    private static MenuOptions menuOptions;
+    private static Scanner input;
+
+    public static void main(String[] args) {
+        input = new Scanner(System.in);
+        menuOptions = new MenuOptions();
+        //menuOptions.menuChoice(input);
+    }
+
+    public static void getMainMenu() {
+        //menuOptions.menuChoice(input);
+    }
+}
